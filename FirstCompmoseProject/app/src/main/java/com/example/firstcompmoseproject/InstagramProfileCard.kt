4f54339e3c4749db894e9d767830f8aefed37248
Dmitry K <fatalzero@Mail.ru>
2023-07-20 16:52:23 +0300

    Dark-Light Theme

--- FirstCompmoseProject/app/src/main/java/com/example/firstcompmoseproject/InstagramProfileCard.kt
+++ FirstCompmoseProject/app/src/main/java/com/example/firstcompmoseproject/InstagramProfileCard.kt
@@ -1,45 +1,53 @@
-package com.example.firstcompmoseproject
-
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.firstcompmoseproject.ui.theme.FirstCompmoseProjectTheme
 
-
-@Preview
 @Composable
 fun InstagramProfileCard() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
+    Card(
+        modifier = Modifier.padding(8.dp),
+        backgroundColor = MaterialTheme.colors.background,
+        shape = RoundedCornerShape(
+            topStart = 4.dp,
+            topEnd = 4.dp
+        ),
+        border = BorderStroke(1.dp, MaterialTheme.colors.onBackground)
     ) {
-        Box(
+        Row(
             modifier = Modifier
-                .size(50.dp)
-                .background(color = Color.Yellow)
-        )
-        TwoBoxes()
-        TwoBoxes()
-        TwoBoxes()
-
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .background(color = Color.Yellow)
+            )
+            TwoBoxes()
+            TwoBoxes()
+            TwoBoxes()
+        }
     }
-
 }
 
 @Composable
-private fun RowScope.TwoBoxes() {
+private fun TwoBoxes() {
     Column(
-        modifier = Modifier
-            .height(80.dp),
+        modifier = Modifier.height(80.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceEvenly
+        verticalArrangement = Arrangement.SpaceEvenly,
     ) {
         Box(
             modifier = Modifier
@@ -54,3 +62,22 @@ private fun RowScope.TwoBoxes() {
     }
 }
 
+@Preview
+@Composable
+fun PreviewCardLight() {
+    FirstCompmoseProjectTheme(
+        darkTheme = false
+    ) {
+        InstagramProfileCard()
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCardDark() {
+    FirstCompmoseProjectTheme(
+        darkTheme = true
+    ) {
+        InstagramProfileCard()
+    }
+}

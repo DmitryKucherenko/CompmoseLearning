b0c41f0f28ac944545fb3138db59c7d82d237bf5
Dmitry K <fatalzero@Mail.ru>
2023-07-20 17:06:51 +0300

    Text

--- FirstCompmoseProject/app/src/main/java/com/example/firstcompmoseproject/MainActivity.kt
+++ FirstCompmoseProject/app/src/main/java/com/example/firstcompmoseproject/MainActivity.kt
@@ -7,7 +7,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.firstcompmoseproject.ui.theme.FirstCompmoseProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,5 +35,40 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun testText() {
+    Column() {
+        Text("Hello World!", fontWeight = FontWeight.Bold, fontSize = 25.sp)
+        Text(buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(
+                    textDecoration = TextDecoration.combine(
+                        listOf(
+                            TextDecoration.Underline,
+                            TextDecoration.LineThrough
+                        )
+                    )
+                )
+            ) {
+               append("He")
+            }
+
+            withStyle(
+                style = SpanStyle(
+                    color = Color.Red,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.ExtraBold
+                    )
+                )
+             {
+                append("llo")
+            }
+
+        })
+    }
+
 }
 

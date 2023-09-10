9ce4f806de556b703195cc7bdc91cae1a991b54c
Dmitry K <fatalzero@Mail.ru>
2023-09-10 19:10:04 +0300

    Autorization VK

--- ActivityResultApi-old-method/app/src/main/java/com/sumin/activityresultapi/MainActivity.kt
+++ ActivityResultApi-old-method/app/src/main/java/com/sumin/activityresultapi/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sumin.activityresultapi
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
@@ -21,49 +22,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initViews()
-        val contact = object : ActivityResultContract<Intent, String?>(){
-            override fun createIntent(context: Context, input: Intent): Intent {
-                return input
+        val contractUserName = ActivityResultContracts.StartActivityForResult()
+
+        val launcherUserName = registerForActivityResult(contractUserName) {
+            if (it.resultCode == RESULT_OK) {
+                usernameTextView.text = it.data?.getStringExtra(UsernameActivity.EXTRA_USERNAME)
             }
-
-            override fun parseResult(resultCode: Int, intent: Intent?): String? {
-                if (resultCode == RESULT_OK) {
-                    return intent?.getStringExtra(UsernameActivity.EXTRA_USERNAME) ?: ""
-                }
-                return null
-            }
-
-
         }
 
-        val launcher = registerForActivityResult(contact){
-            if(!it.isNullOrBlank()){
-                usernameTextView.text = it
-            }
+        val contractImage = ActivityResultContracts.GetContent()
+
+
+
+        val launcherImage = registerForActivityResult(contractImage) {
+            imageFromGalleryImageView.setImageURI(it)
         }
 
         getUsernameButton.setOnClickListener {
-           launcher.launch(UsernameActivity.newIntent(this))
+            launcherUserName.launch(UsernameActivity.newIntent(this))
         }
         getImageButton.setOnClickListener {
-            Intent(Intent.ACTION_PICK).apply {
-                type = "image/*" // MIME types
-                startActivityForResult(this, RC_IMAGE)
-            }
+            launcherImage.launch("image/*")
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == RC_USERNAME && resultCode == RESULT_OK) {
-            val username = data?.getStringExtra(UsernameActivity.EXTRA_USERNAME) ?: ""
-            usernameTextView.text = username
-        }
-        if (requestCode == RC_IMAGE && resultCode == RESULT_OK) {
-            val uri = data?.data
-            imageFromGalleryImageView.setImageURI(uri)
-        }
-    }
 
     private fun initViews() {
         getUsernameButton = findViewById(R.id.get_username_button)
@@ -72,11 +54,6 @@ class MainActivity : AppCompatActivity() {
         imageFromGalleryImageView = findViewById(R.id.image_from_gallery_imageview)
     }
 
-    companion object {
-
-        private const val RC_USERNAME = 100
-        private const val RC_IMAGE = 101
-    }
 }
 
 
--- VkClientNews/app/src/main/java/com/example/vkclientnews/MainActivity.kt
+++ VkClientNews/app/src/main/java/com/example/vkclientnews/MainActivity.kt
@@ -2,22 +2,39 @@ package com.example.vkclientnews
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 
 import androidx.lifecycle.ViewModelProvider
+import com.example.vkclientnews.ui.theme.ActivityResultTest
 import com.example.vkclientnews.ui.theme.MainScreen
 import com.example.vkclientnews.ui.theme.VkClientNewsTheme
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-           VkClientNewsTheme() {
-               MainScreen()
-           }
-
+            VkClientNewsTheme {
+                val launcher =
+                    rememberLauncherForActivityResult(contract = VK.getVKAuthActivityResultContract()) {
+                        when (it) {
+                            is VKAuthenticationResult.Success -> {
+                                Log.d("MainActivity", "Success auth")
+                            }
+                            is VKAuthenticationResult.Failed -> {
+                                Log.d("MainActivity", "Failed auth")
+                            }
+                        }
+                    }
+                launcher.launch(listOf(VKScope.WALL))
+                MainScreen()
+            }
         }
     }
 }

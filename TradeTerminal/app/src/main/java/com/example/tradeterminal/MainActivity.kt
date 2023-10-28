0f730ca8d8ffd2cd483d9e4a23d5dbd335bf4d5d
Dmitry K <fatalzero@Mail.ru>
2023-10-28 18:40:40 +0300

    Initial commit

---
+++ TradeTerminal/app/src/main/java/com/example/tradeterminal/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.tradeterminal
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tradeterminal.ui.theme.TradeTerminalTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TradeTerminalTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TradeTerminalTheme {
+        Greeting("Android")
+    }
+}

a254ea7cbb1aaed8e05ca1cf6a14c7b649400042
Dmitry K <fatalzero@Mail.ru>
2023-11-04 16:19:23 +0300

    remember Terminal State

--- TradeTerminal/app/src/main/java/com/example/tradeterminal/presentation/Terminal.kt
+++ TradeTerminal/app/src/main/java/com/example/tradeterminal/presentation/Terminal.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.gestures.TransformableState
 import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.layout.onSizeChanged
 import com.example.tradeterminal.data.Bar
 import kotlin.math.roundToInt
 
@@ -17,41 +19,23 @@ private const val MIN_VISIBLE_BARS_COUNT = 20
 
 @Composable
 fun Terminal(bars: List<Bar>) {
-    var visibleBarsCount by remember {
-        mutableStateOf(100)
-    }
 
 
-    var terminalWidth by remember {
-        mutableStateOf(0f)
-    }
-
-    val barWidth by remember {
-        derivedStateOf {
-            terminalWidth / visibleBarsCount
-        }
-    }
+    var terminalState by rememberTerminalState(bars =bars)
 
 
-    var scrolledBy by remember {
-        mutableStateOf(0f)
-    }
-
-    val visibleBars by remember {
-        derivedStateOf {
-            val startIndex = (scrolledBy/barWidth).roundToInt().coerceAtLeast(0)
-            val endIndex = (startIndex + visibleBarsCount).coerceAtMost(bars.size)
-            bars.subList(startIndex,endIndex)
-        }
-    }
 
     val transformableState = TransformableState { zoomChange, panChange, _ ->
-        visibleBarsCount = (visibleBarsCount / zoomChange).roundToInt()
+        val visibleBarsCount = (terminalState.visibleBarsCount / zoomChange).roundToInt()
             .coerceIn(MIN_VISIBLE_BARS_COUNT, bars.size)
 
-        scrolledBy = (scrolledBy +  panChange.x)
+        val scrolledBy = (terminalState.scrolledBy +  panChange.x)
             .coerceAtLeast(0f)
-            .coerceAtMost(bars.size*barWidth - terminalWidth)
+            .coerceAtMost(bars.size*terminalState.barWidth - terminalState.terminalWidth)
+        terminalState = terminalState.copy(
+            visibleBarsCount = visibleBarsCount,
+            scrolledBy = scrolledBy
+        )
 
     }
     Canvas(
@@ -59,13 +43,18 @@ fun Terminal(bars: List<Bar>) {
             .fillMaxSize()
             .background(Color.Black)
             .transformable(transformableState)
+            .onSizeChanged {
+                terminalState = terminalState.copy(terminalWidth = it.width.toFloat())
+            }
     ) {
-        terminalWidth = size.width
-        val max = visibleBars.maxOf { it.high }
-        val min = visibleBars.minOf { it.low }
+
+        val max = terminalState.visibleBars.maxOf { it.high }
+
+        val min = terminalState.visibleBars.minOf { it.low }
+
         val pxPerPoint = size.height / (max - min)
-        translate(left = scrolledBy){ bars.forEachIndexed { index, bar ->
-            val offsetX = size.width - index * barWidth
+        translate(left = terminalState.scrolledBy){ bars.forEachIndexed { index, bar ->
+            val offsetX = size.width - index * terminalState.barWidth
             drawLine(
                 color = Color.White,
                 start = Offset(offsetX, size.height - (bar.low - min) * pxPerPoint),
@@ -81,7 +70,7 @@ fun Terminal(bars: List<Bar>) {
                 },
                 start = Offset(offsetX, size.height - (bar.open - min) * pxPerPoint),
                 end = Offset(offsetX, size.height - (bar.close - min) * pxPerPoint),
-                strokeWidth = barWidth/2
+                strokeWidth = terminalState.barWidth/2
             )
 
         }}
---
+++ TradeTerminal/app/src/main/java/com/example/tradeterminal/presentation/TerminalState.kt
@@ -0,0 +1,34 @@
+package com.example.tradeterminal.presentation
+
+import android.os.Parcelable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import com.example.tradeterminal.data.Bar
+import kotlinx.parcelize.Parcelize
+import kotlin.math.roundToInt
+
+@Parcelize
+data class TerminalState(
+    val barList:List<Bar>,
+    val visibleBarsCount: Int = 100,
+    val terminalWidth: Float = 0f,
+    val scrolledBy: Float = 0f
+):Parcelable {
+    val barWidth: Float
+        get() = terminalWidth / visibleBarsCount
+
+    val visibleBars: List<Bar>
+        get() {
+            val startIndex = (scrolledBy / barWidth).roundToInt().coerceAtLeast(0)
+            val endIndex = (startIndex + visibleBarsCount).coerceAtMost(barList.size)
+            return barList.subList(startIndex, endIndex)
+        }
+}
+
+@Composable
+fun rememberTerminalState(bars:List<Bar>): MutableState<TerminalState> {
+    return rememberSaveable {
+        mutableStateOf(TerminalState(bars))
+    }
+}
+
